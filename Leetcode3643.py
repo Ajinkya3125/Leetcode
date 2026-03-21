aa5ce028db3ce70ff6925095afccfb573ac9b231
Ajinkya3125 <sajinkya650@gmail.com>
2026-03-21 23:32:48 +0530

    Python file

---
+++ Leetcode3643.py
@@ -0,0 +1,16 @@
+# You are given an m x n integer matrix grid, and three integers x, y, and k.
+# The integers x and y represent the row and column indices of the top-left corner of a
+# square submatrix and the integer k represents the size (side length) of the square submatrix.
+# Your task is to flip the submatrix by reversing the order of its rows vertically.
+# Return the updated matrix.
+
+from typing import List
+
+class Solution:
+    def reverseSubmatrix(self, grid: List[List[int]], x: int, y: int, k: int) -> List[List[int]]:
+        for i in range(k // 2):
+            for j in range(k):
+                grid[x + i][y + j], grid[x + k - 1 - i][y + j] = \
+                grid[x + k - 1 - i][y + j], grid[x + i][y + j]
+        
+        return grid

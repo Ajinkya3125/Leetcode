db542aaf2b9b9e3845322525bb359dbe69c3afe9
Ajinkya3125 <sajinkya650@gmail.com>
2026-03-26 22:56:28 +0530

    Python file

---
+++ Leetcode219.py
@@ -0,0 +1,15 @@
+#219:Contains Duplicates II
+
+from typing import List
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        index_map = {}  # value -> last index
+        
+        for i, num in enumerate(nums):
+            if num in index_map:
+                if i - index_map[num] <= k:
+                    return True
+            index_map[num] = i
+        
+        return False

188560e16c5ac41b718ff60799378df2d4354444
Ajinkya3125 <sajinkya650@gmail.com>
2026-04-09 22:45:08 +0530

    Python file

---
+++ Leetcode521.py
@@ -0,0 +1,7 @@
+#521:Longest Uncommon Subsequence - 1
+
+class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        if a == b:
+            return -1
+        return max(len(a),len(b))

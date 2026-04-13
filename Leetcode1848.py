ed6378bf90fffd051bc7e92319398476206768a7
Ajinkya3125 <sajinkya650@gmail.com>
2026-04-13 23:06:31 +0530

    Python file

---
+++ Leetcode1848.py
@@ -0,0 +1,13 @@
+#1848:Minimum distance to the Target element
+
+from typing import List
+
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        min_distance = float('inf')
+        
+        for i in range(len(nums)):
+            if nums[i] == target:
+                min_distance = min(min_distance, abs(i - start))
+        
+        return min_distance

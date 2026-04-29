e46d3a2593e07c6e0fbf90dcf8e72b4ea121d571
Ajinkya3125 <sajinkya650@gmail.com>
2026-04-29 23:26:33 +0530

    Python file

---
+++ Leetcode228.py
@@ -0,0 +1,30 @@
+##228 : Summary Ranges
+
+from typing import List
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result = []
+        
+        if not nums:
+            return result
+        
+        start = nums[0]
+        
+        for i in range(1, len(nums)):
+            # If not consecutive
+            if nums[i] != nums[i - 1] + 1:
+                if start == nums[i - 1]:
+                    result.append(str(start))
+                else:
+                    result.append(f"{start}->{nums[i - 1]}")
+                
+                start = nums[i]
+        
+        # Handle last range
+        if start == nums[-1]:
+            result.append(str(start))
+        else:
+            result.append(f"{start}->{nums[-1]}")
+        
+        return result

6f0871995af2504179d6f7bdb040b8b113ca70a0
Ajinkya3125 <sajinkya650@gmail.com>
2026-05-11 23:28:30 +0530

    Python file

---
+++ Leetcode2553.py
@@ -0,0 +1,10 @@
+##2553 : Seperate the elements in an array
+
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        result = []
+
+        for num in nums:
+            for digit in str(num):
+                result.append(int(digit))
+        return result

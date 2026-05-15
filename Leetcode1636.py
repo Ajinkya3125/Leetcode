10f3125d777f6b0c0ec8800b657f5a755b592514
Ajinkya3125 <sajinkya650@gmail.com>
2026-05-15 21:40:08 +0530

    Python file

---
+++ Leetcode1636.py
@@ -0,0 +1,9 @@
+##1636 : Sort array by Increasing Frequency
+
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        freq = {}
+        for n in nums:
+            freq[n] = freq.get(n,0) + 1
+
+        return sorted(nums, key=lambda x:(freq[x],-x))

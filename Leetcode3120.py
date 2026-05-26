00e27e8aba3d386637a03bba38f3976f68893e58
Ajinkya3125 <sajinkya650@gmail.com>
2026-05-26 17:51:18 +0530

    Python file

---
+++ Leetcode3120.py
@@ -0,0 +1,13 @@
+##3120 : Count the number of special characters I
+
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        lower = set()
+        upper = set()
+
+        for ch in word:
+            if ch.islower():
+                lower.add(ch)
+            else:
+                upper.add(ch.lower())
+        return len(lower & upper)

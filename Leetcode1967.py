6708c0c5b1437decb510dffa2095d534611c272b
Ajinkya3125 <sajinkya650@gmail.com>
2026-06-12 15:56:05 +0530

    Python file

---
+++ Leetcode1967.py
@@ -0,0 +1,9 @@
+#1967 : Number of Strings That Appear as Substrings in Word.
+
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        cnt = 0
+        for p in patterns:
+            if p in word:
+                cnt += 1
+        return cnt

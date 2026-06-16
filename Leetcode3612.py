00ec30c247a2e87e7fa458a3d0aaa11d4bf536b8
Ajinkya3125 <sajinkya650@gmail.com>
2026-06-16 23:55:07 +0530

    Python file

---
+++ Leetcode3612.py
@@ -0,0 +1,21 @@
+#3612 : Process String with Special Operation I
+
+class Solution:
+    def processStr(self, s: str) -> str:
+        result = []
+
+        for ch in s:
+            if ch.isalpha():
+                result.append(ch)
+
+            elif ch == "*":
+                if result:
+                    result.pop()
+
+            elif ch == "#":
+                result.extend(result)
+
+            elif ch == "%":
+                result.reverse()
+
+        return "".join(result)

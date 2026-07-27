aef1de1bad8d838de040ac6b8765cef8abf14af8
Ajinkya3125 <sajinkya650@gmail.com>
2026-07-27 22:45:28 +0530

    Python file

---
+++ Leetcode1464.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        largest = second = 0
+
+        for num in nums:
+            if num > largest:
+                second = largest
+                largest = num
+            elif num > second:
+                second = num
+
+        return (largest - 1) * (second - 1)

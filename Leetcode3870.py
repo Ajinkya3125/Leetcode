66096f3388eddaae8f828b16e1b2799edbc29973
Ajinkya3125 <sajinkya650@gmail.com>
2026-09-08 23:43:17 +0530

    Python file

---
+++ Leetcode3870.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        cnt = 0
+        if n >= 1000:
+            cnt += n - 999
+
+        if n >= 1000000:
+            cnt += n - 999999
+
+        if n >= 1000000000:
+            cnt += n - 999999999
+
+        return cnt

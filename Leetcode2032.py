6ba5feec833b9907503f2ff693a0b84c8c11b21f
Ajinkya3125 <sajinkya650@gmail.com>
2026-09-18 23:59:25 +0530

    Python file

---
+++ Leetcode2032.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoOutOfThree(self, nums1: list[int], nums2: list[int], nums3: list[int]) -> list[int]:
+        s1 = set(nums1)
+        s2 = set(nums2)
+        s3 = set(nums3)
+
+        ans = (s1 & s2) | (s1 & s3) | (s2 & s3)
+
+        return list(ans)
